1ae1358d432ee1424e55c7e560d8f2f06919e286
Alex_Wang <k8cchaha@gmail.com>
2022-04-24 19:26:18 +0800

    req & res

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 const http = require("http");
 
 const requestListener = (req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.write("Hello Node");
-  res.end();
+  const header = { "Content-Type": "text/plain" };
+
+  if (req.url === "/" && req.method === "GET") {
+    res.writeHead(200, header);
+    res.write("Hello Node");
+    res.end();
+  } else if (req.url === "/" && req.method === "DELETE") {
+    res.writeHead(200, header);
+    res.write("Delete!");
+    res.end();
+  } else {
+    res.writeHead(404, header);
+    res.write("Not Found");
+    res.end();
+  }
 };
 
 const server = http.createServer(requestListener);
